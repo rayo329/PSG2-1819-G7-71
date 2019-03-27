6fbeca3b3337d72d8cdfbd0733d0f2a12010f95e
rayo329 <rayo329@hotmail.com>
2019-03-27 16:25:53 +0100

    Completada la tarea de mostrar detalles de la causa

--- src/main/java/org/springframework/samples/petclinic/web/CauseController.java
+++ src/main/java/org/springframework/samples/petclinic/web/CauseController.java
@@ -39,6 +39,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Juergen Hoeller
@@ -80,6 +81,13 @@ public class CauseController {
             this.clinicService.saveCause(cause);
             return "redirect:/";
         }
+    }
+	
+	@RequestMapping("/causes/{causeId}")
+    public ModelAndView showCause(@PathVariable("causeId") int causeId) {
+        ModelAndView mav = new ModelAndView("causes/causeDetails");
+        mav.addObject(this.clinicService.findCauseById(causeId));
+        return mav;
     }
 
 }
