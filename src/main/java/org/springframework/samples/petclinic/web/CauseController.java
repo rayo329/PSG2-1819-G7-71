152e91fb868777597fb70b0736f7c1f90f509e62
Barranco <rayo329@hotmail.com>
2019-04-02 17:13:24 +0200

    cambio cause Controller

--- src/main/java/org/springframework/samples/petclinic/web/CauseController.java
+++ src/main/java/org/springframework/samples/petclinic/web/CauseController.java
@@ -83,7 +83,7 @@ public class CauseController {
         }
     }
 	
-	@RequestMapping("/causes/{causeId}")
+	@RequestMapping("/causes/{causeId}/details")
     public ModelAndView showCause(@PathVariable("causeId") int causeId) {
         ModelAndView mav = new ModelAndView("causes/causeDetails");
         mav.addObject(this.clinicService.findCauseById(causeId));
